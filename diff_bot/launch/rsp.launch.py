764f38fe59099c71572768b49b71f58fc572444c
mohammadalhadiayash <mohammadalhadiayash@gmail.com>
2024-07-27 05:22:47 +0300

    gazebo

--- diff_bot/launch/rsp.launch.py
+++ diff_bot/launch/rsp.launch.py
@@ -10,36 +10,36 @@ import xacro
 def generate_launch_description():
 
     ####### DATA INPUT ##########
-    urdf_file_name = 'robot.urdf'
+    urdf_file_name = 'robot.urdf.xacro'
     package_name= "diff_bot"
 
     ####### DATA INPUT END ##########
     print("Fetching URDF ==>")
     # path_to_our_package/urdf/urdf_file
     robot_desc_path = os.path.join(get_package_share_directory(package_name), "urdf", urdf_file_name)
-    urdf_content = open(robot_desc_path).read()
-    #xacro_file = xacro.process_file(robot_desc_path)
+    xacro_file = xacro.process_file(robot_desc_path)
     
     # Robot State Publisher
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher_node',
-        parameters=[{'use_sim_time': True, 'robot_description': urdf_content}],
+        parameters=[{'use_sim_time': True, 'robot_description': xacro_file.toxml()}],
         output="screen"
     )
     
     # joint State Publisher
-    # joint_state_publisher_node = Node(
-    #     package='joint_state_publisher',
-    #     executable='joint_state_publisher',
-    #     name='joint_state_publisher_node',
-    #     output="screen"
-    # )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher_node',
+        output="screen"
+    )
 
     # create and return launch description object
     return LaunchDescription(
         [            
             robot_state_publisher_node,
+            joint_state_publisher_node
         ]
     )
